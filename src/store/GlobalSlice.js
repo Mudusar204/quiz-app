fcb51c43a4399e19bfa167b403990b74d3883841
Mudusar Arshad <hafizmudusar37@gmail.com>
2024-03-10 15:41:52 +0500

    initial commit

---
+++ src/store/GlobalSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const GlobalSlice = createSlice({
+  name: "GlobalSlice",
+  initialState: {
+    loader: false,
+  },
+  reducers: {
+    
+  },
+});
+
